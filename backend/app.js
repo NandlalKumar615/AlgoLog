8e54fd4bd37c3a6947ee3b800ac0c56045d1a3f9
NandlalKumar615 <nandlalkumar615@gmail.com>
2023-12-31 12:28:57 +0530

    u3

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
 const errorMiddleware = require("./middleware/error");
-const path = require("path")
+// const path = require("path")
 
 
 app.use(express.json());
@@ -10,7 +10,7 @@ app.use(cookieParser());
 
 //config
 if(process.env.NODE_ENVIRONMENT !== "PRODUCTION"){
-    require("dotenv").config({path:"backend/config/config.env"});
+    require("dotenv").config({path:"config/config.env"});
 }
 
 // Import Route
@@ -21,11 +21,11 @@ const usersRoutes = require("./routes/userRoute");
 app.use("/api/v1",notesRoutes);
 app.use("/api/v1",usersRoutes);
 
-app.use(express.static(path.join(__dirname, "../frontend/build")));
+// app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-app.get("*", (req,res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
-});
+// app.get("*", (req,res) => {
+//     res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+// });
 
 
 app.use(errorMiddleware);
