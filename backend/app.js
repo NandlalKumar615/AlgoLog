ef985c01318879236df50c57f983054712ad7f07
NandlalKumar615 <nandlalkumar615@gmail.com>
2023-12-31 13:36:00 +0530

    u7

--- backend/app.js
+++ backend/app.js
@@ -2,11 +2,20 @@ const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
 const errorMiddleware = require("./middleware/error");
+const cors = require("cors");
 // const path = require("path")
 
 
 app.use(express.json());
 app.use(cookieParser());
+
+app.use(cors(
+    {
+        origin: ["https://algo-log-snpg.vercel.app"],
+        method: ["POST", "GET"],
+        credentials: true
+    }
+))
 
 //config
 if(process.env.NODE_ENVIRONMENT !== "PRODUCTION"){
